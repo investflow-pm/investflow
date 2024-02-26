a767d8ba2a8e235d4557d07b68e697cbb7449324
razondark <bryakovdima@yandex.ru>
2024-02-26 20:07:55 +0300

    init bot

---
+++ tg_bot/src/main/java/ru/tinkoff/tinvestments/config/TelegramBotConfig.java
@@ -0,0 +1,16 @@
+package ru.tinkoff.tinvestments.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramBotConfig {
+    @Value("${bot.name}")
+    @Getter
+    private String botName;
+
+    @Value("${bot.token}")
+    @Getter
+    private String botToken;
+}
