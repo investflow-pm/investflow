e49aba6c1f694fe6f4e73c3f157669308e29a0a8
Yaroslav <76592052+honeyfuc@users.noreply.github.com>
2024-02-27 22:02:13 +0300

    Fixed bug with Mapper

--- auth-microservice/src/main/java/com/yaroslavyankov/authmicroservice/web/mappers/UserMapper.java
+++ auth-microservice/src/main/java/com/yaroslavyankov/authmicroservice/web/mappers/UserMapper.java
@@ -5,7 +5,6 @@ import com.yaroslavyankov.authmicroservice.web.dto.user.UserDto;
 import org.mapstruct.Mapper;
 
 
-
 @Mapper(componentModel = "spring")
 public interface UserMapper extends Mappable<User, UserDto> {
 
