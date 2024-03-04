b1629e88d391c0d88f776ceceb7f41b85e80cc2b
razondark <bryakovdima@yandex.ru>
2024-03-04 22:35:39 +0300

    clear pom.xml and add bot key

--- tg-bot/src/main/java/ru/tinkoff/tinvestments/TInvestmentsApplication.java
+++ tg-bot/src/main/java/ru/tinkoff/tinvestments/TInvestmentsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TInvestmentsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TInvestmentsApplication.class, args);
 	}
