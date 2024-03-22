deaab65b3e733acee571ecab1202847ee43323e8
Yaroslav <76592052+honeyfuc@users.noreply.github.com>
2024-03-22 21:38:27 +0300

    added api gateway

--- api-gateway/src/main/java/com/yaroslavyankov/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/yaroslavyankov/apigateway/ApiGatewayApplication.java
@@ -1,12 +1,18 @@
-package ru.tinkoff.tinvestments;
+package com.yaroslavyankov.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class TInvestmentsApplication {
+@EnableDiscoveryClient
+@Configuration
+public class ApiGatewayApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(TInvestmentsApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
 }
